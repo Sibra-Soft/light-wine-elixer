f0925ae428aa01c99bea37c9e46fd06e6c39e090
Alex van den Berg <info@sibra-soft.nl>
2022-06-25 10:41:18 +0200

    Added js-templater Javascript script

--- src/Core/Scripts/js-templater.js
+++ src/Core/Scripts/js-templater.js
@@ -1,5 +1,41 @@
 ﻿class JsTemplater {
-    Render(object, container) {
+    async Render(requestPath, selector) {
+        return new Promise(async (resolve, reject) => {
+            const request = await elixer.request.get(requestPath);
 
+            if (request.status == 200) {
+                const items = JSON.parse(request.data);
+
+                const template = $(selector);
+                const compiledTemplate = Template7.compile(template.html());
+                const html = compiledTemplate(items);
+
+                if (template.data("replace")) {
+                    $(selector).replaceWith(html);
+                } else {
+                    $(selector).nextAll().remove();
+                    $(selector).after(html);
+                }
+
+                resolve();
+            } else {
+                reject();
+            }
+        });
+    }
+
+    async RenderFromObject(object, selector) {
+        const items = object;
+
+        const template = $(selector);
+        const compiledTemplate = Template7.compile(template.html());
+        const html = compiledTemplate(items);
+
+        if (template.data("replace")) {
+            $(selector).replaceWith(html);
+        } else {
+            $(selector).nextAll().remove();
+            $(selector).after(html);
+        }
     }
 }
