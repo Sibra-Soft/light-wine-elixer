760e696394a2a63d5ad19b7f9743d41af7b710bd
Alex van den Berg <info@sibra-soft.nl>
2022-06-25 10:44:22 +0200

    Added views Javascript

---
+++ src/Core/Scripts/views.js
@@ -0,0 +1,11 @@
+﻿class Views {
+    Show(name) {
+        $(".view-container").addClass("hide");
+        $(`.view-container#${name}`).removeClass("hide");
+    }
+
+    Close() {
+        $(".view-container").addClass("hide");
+        $(".view-container#main").removeClass("hide");
+    }
+}
