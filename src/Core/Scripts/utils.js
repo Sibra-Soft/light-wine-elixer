3ad4a52f3edfc40555cb54a46cc28c7c077451db
Alex van den Berg <info@sibra-soft.nl>
2022-07-15 12:44:40 +0200

    Added function for getting the querystring

--- src/Core/Scripts/utils.js
+++ src/Core/Scripts/utils.js
@@ -1,4 +1,15 @@
 ﻿class Utils {
+    getQuerystring() {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for (var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
+
     getCookie(name) {
         const value = `; ${document.cookie}`;
         const parts = value.split(`; ${name}=`);
