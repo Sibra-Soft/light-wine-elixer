2bff2096260144a1efa5b2175dc08670f80e4b7b
Alex van den Berg <info@sibra-soft.nl>
2022-07-16 20:47:14 +0200

    Bugfix for interfaces

--- src/Core/Interfaces/IPageService.php
+++ src/Core/Interfaces/IPageService.php
@@ -1,6 +1,9 @@
 <?php
 namespace Elixer\Core\Interfaces;
 
+use LightWine\Core\Models\PageModel;
+use LightWine\Modules\Routing\Models\RouteModel;
+
 interface IPageService
 {
     public function Render(RouteModel $route): PageModel;
--- src/Core/Interfaces/IRoutingService.php
+++ src/Core/Interfaces/IRoutingService.php
@@ -1,6 +1,8 @@
 <?php
 namespace Elixer\Core\Interfaces;
 
+use LightWine\Modules\Routing\Models\RouteModel;
+
 interface IRoutingService
 {
     public function MatchRouteByUrl(string $url): RouteModel;
