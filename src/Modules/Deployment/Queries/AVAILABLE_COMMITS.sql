452faf372f9c57432a579a65d007b21f8541a517
Alex van den Berg <info@sibra-soft.nl>
2022-08-29 10:49:53 +0200

    Changed query for getting commints

--- src/Modules/Deployment/Queries/AVAILABLE_COMMITS.sql
+++ src/Modules/Deployment/Queries/AVAILABLE_COMMITS.sql
@@ -5,8 +5,8 @@ SELECT
 	template.template_version_test AS `test_version`,
 	template.template_version_dev AS `current_version`,
 	template.type,
-	DATE_FORMAT(IFNULL(template.date_modified, ''), '%d-%m-%Y %H:%i:%s') AS date_modified,
-	template.modified_by
+	DATE_FORMAT(IFNULL(template.date_modified, template.date_added), '%d-%m-%Y %H:%i:%s') AS date_modified,
+	IFNULL(template.modified_by, template.created_by) AS modified_by
 FROM `site_templates` AS template
 LEFT JOIN _commits_objects AS commit_objects ON commit_objects.template_id = template.id AND commit_objects.template_version = template.template_version_dev
 WHERE template.type NOT IN ('folder')
